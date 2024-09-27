ae52f5e90eff4992fadd0e16d476f0f3bcc2832d
Tienduy2004 <duyphung344@gmail.com>
2024-09-27 14:29:00 +0700

    Allocate folder structure and bring templates and projects

---
+++ Travel/app/Http/Controllers/HomeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('home.index'); 
+    }
+    public function about()
+    {
+        return view('home.about'); 
+    }
+    public function service()
+    {
+        return view('home.service'); 
+    }
+    public function tour()
+    {
+        return view('home.package'); 
+    }
+    public function contact()
+    {
+        return view('home.contact'); 
+    }
+    //page
+    public function blog()
+    {
+        return view('home.page.blog'); 
+    }
+    public function destination()
+    {
+        return view('home.page.destination'); 
+    }
+    public function guide()
+    {
+        return view('home.page.guide'); 
+    }
+    public function single()
+    {
+        return view('home.page.single'); 
+    }
+    public function testimonial()
+    {
+        return view('home.page.testimonial'); 
+    }
+}
--- Travel/routes/web.php
+++ Travel/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [HomeController::class, 'index'])->name("home");
+Route::get('/about', [HomeController::class, 'about'])->name("about");
+Route::get('/service', [HomeController::class, 'service'])->name("service");
+Route::get('/tour', [HomeController::class, 'tour'])->name("tour");
+Route::get('/contact', [HomeController::class, 'contact'])->name("contact");
+//page
+Route::get('/blog', [HomeController::class, 'blog'])->name("blog");
+Route::get('/single', [HomeController::class, 'single'])->name("single");
+Route::get('/destination', [HomeController::class, 'destination'])->name("destination");
+Route::get('/guide', [HomeController::class, 'guide'])->name("guide");
+Route::get('/testimonial', [HomeController::class, 'testimonial'])->name("testimonial");
