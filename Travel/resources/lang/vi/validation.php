c56ead199850a440a1dbcbf9ff838568e25d74cf
DiDinhKhoi <didinhkhoi.tdc2020@gmail.com>
2024-10-19 13:32:28 +0700

    validation

--- Travel/resources/lang/vi/validation.php
+++ Travel/resources/lang/vi/validation.php
@@ -3,6 +3,10 @@
 return [
     'required' => ':attribute là bắt buộc.',
     'string' => ':attribute phải là chuỗi.',
+    'min' => [
+        'string' => ':attribute phải có ít nhất :min ký tự.',
+        'numeric' => ':attribute phải lớn hơn hoặc bằng :min.',
+        'file' => ':attribute phải lớn hơn :min kilobyte.'],
     'max' => [
         'string' => ':attribute không được vượt quá :max ký tự.',
         'file' => ':attribute không được vượt quá :max kilobyte.',
