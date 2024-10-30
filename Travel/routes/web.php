1153d0a57e1c4ae700302de921feae0f7d3d4631
Tienduy2004 <duyphung344@gmail.com>
2024-10-30 18:41:55 +0700

    update

---
+++ Travel/routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+use App\Http\Controllers\Auth\OTPVefificationController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Auth\VerifyOTPController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\TourController;
+
+Route::get('/', [HomeController::class, 'index'])->name("home");
+Route::get('/about', [HomeController::class, 'about'])->name("about");
+Route::get('/service', [HomeController::class, 'service'])->name("service");
+Route::get('/tours', [HomeController::class, 'tour'])->name("tour");
+Route::get('/contact', [HomeController::class, 'contact'])->name("contact");
+
+
+//page
+Route::get('/blog', [HomeController::class, 'blog'])->name("blog");
+Route::get('/single', [HomeController::class, 'single'])->name("single");
+Route::get('/destination', [HomeController::class, 'destination'])->name("destination");
+Route::get('/guide', [HomeController::class, 'guide'])->name("guide");
+Route::get('/testimonial', [HomeController::class, 'testimonial'])->name("testimonial");
+//login
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+require __DIR__ . '/auth.php';
+Route::get('/verify-otp', [VerifyOTPController::class, 'showVerifyForm'])->middleware('auth')->name('otp.verify');
+Route::post('/verify-otp', [VerifyOTPController::class, 'verify'])->middleware('auth')->name('otp.verify.post');
+Route::post('/otp/resend', [OTPVefificationController::class, 'resend'])->name('otp.resend');
+
+//tour
+Route::get('/tours/{slug}', [TourController::class, 'show'])->name('tours.show');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/booking/{id}', [TourController::class, 'showBookingPage'])->name('tours.booking');
+    Route::post('/booking', [BookingController::class, 'store'])->name('bookings.store');
+    Route::get('/booking-payment/{bookingCode}', [PaymentController::class, 'showPaymentForm'])->name('booking.payment');
+    Route::post('/payment', [PaymentController::class, 'createPaymentLink'])->name('payment.create');
+    Route::post('/booking', [BookingController::class, 'cancel'])->name('bookings.cancel');
+    Route::get('/payment/cancel', [PaymentController::class, 'cancelPaymentLink']);
+    Route::get('/payment/success', [PaymentController::class, 'successPaymentLink']);
+});
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'profile'])->name('profile');
+    
+});
