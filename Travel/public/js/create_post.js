ba8580957538bb5c9e7b6c33cc2ba9c4aa241da2
Nguyễn Hoàng Anh Tuấn <104258893+obito39x@users.noreply.github.com>
2024-11-23 00:33:49 +0700

    update

--- Travel/public/js/create_post.js
+++ Travel/public/js/create_post.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         toolbar: [
             ["style", ["style"]],
             ["font", ["bold", "underline", "clear"]],
-            ["color", ["color"]],  // Thanh công cụ màu
+            ["color", ["color"]], // Thanh công cụ màu
             ["para", ["ul", "ol", "paragraph"]],
             ["table", ["table"]],
             ["insert", ["link", "picture", "video"]],
@@ -45,4 +45,40 @@ $(document).ready(function () {
 
     //     alert("Blog post submitted successfully!");
     // });
+});
+$(document).ready(function () {
+    // Giới hạn ký tự cho Title
+    const maxTitleChars = 255;
+    $("#title").on("input", function () {
+        const $this = $(this);
+        const currentLength = $this.val().length;
+
+        if (currentLength > maxTitleChars) {
+            Swal.fire(
+                "Notification",
+                `You can enter up to ${maxTitleChars} characters.`,
+                "warning"
+            );
+            $this.val($this.val().substring(0, maxTitleChars));
+        }
+    });
+
+    const maxContentChars = 1000;
+    $('#content').summernote({
+        height: 300, // Chiều cao của editor
+        callbacks: {
+            onChange: function (contents, $editable) {
+                const textLength = contents.replace(/<[^>]*>/g, "").length; // Loại bỏ thẻ HTML để tính ký tự
+
+                if (textLength > maxContentChars) {
+                    const truncatedText = contents
+                        .replace(/<[^>]*>/g, "") // Loại bỏ thẻ HTML
+                        .substring(0, maxContentChars); // Cắt chuỗi thừa
+
+                    const newContent = $('<div>').text(truncatedText).html();
+                    $('#content').summernote('code', newContent); 
+                }
+            }
+        }
+    });
 });
